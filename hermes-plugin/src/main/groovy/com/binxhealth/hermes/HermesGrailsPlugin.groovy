e9f4275bee3d98452fdc13ef3d4e72fd295d75fc
Andrew <ajcann@gmail.com>
2018-10-12 14:36:33 -0700

    Update some organization references

--- hermes-plugin/src/main/groovy/com/binxhealth/hermes/HermesGrailsPlugin.groovy
+++ hermes-plugin/src/main/groovy/com/binxhealth/hermes/HermesGrailsPlugin.groovy
@@ -9,22 +9,22 @@ class HermesGrailsPlugin extends Plugin {
 
     def title = "Hermes" // Headline display name of the plugin
     def author = "Maura Warner"
-    def authorEmail = "maura.warner@atlasgenetics.com"
+    def authorEmail = "maura.warner@mybinxhealth.com"
     def description = '''\
 An HTTP messenger that sends REST requests to third party APIs with guaranteed one-time delivery
 '''
     def profiles = ['web']
 
     // URL to the plugin's documentation
-    def documentation = "https://github.com/atlasgenetics/hermes/blob/master/README.md"
+    def documentation = "https://github.com/binxhealth/hermes/blob/master/README.md"
 
     // Details of company behind the plugin (if there is one)
-    def organization = [ name: "Atlas Genetics", url: "http://atlasgenetics.com" ]
+    def organization = [ name: "Binx Health", url: "http://mybinxhealth.com" ]
 
     // Location of the plugin's issue tracker.
-    def issueManagement = [ system: "Git", url: "https://github.com/atlasgenetics/hermes/issues" ]
+    def issueManagement = [ system: "Git", url: "https://github.com/binxhealth/hermes/issues" ]
 
     // Online location of the plugin's browseable source code.
-    def scm = [ url: "https://github.com/atlasgenetics/hermes" ]
+    def scm = [ url: "https://github.com/binxhealth/hermes" ]
 
 }
